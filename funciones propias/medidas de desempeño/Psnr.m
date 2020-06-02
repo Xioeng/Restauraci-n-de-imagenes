b2715480d139e7ceeebe12b647eadc7e69d23e1d
Xioeng <64850249+Xioeng@users.noreply.github.com>
2020-06-01 19:29:40 -0500

    Add files via upload

--- funciones propias/medidas de desempeño/Psnr.m
+++ funciones propias/medidas de desempeño/Psnr.m
@@ -1,4 +1,12 @@
 function a=Psnr(Im,Ref)
+%  Calcula el PSNR entre una imagen y una de referencia
+%Datos de entrada: Im, imagen a calular con respecto a la imagen de
+%referencia Ref.
+%Datos de salida: a:PSNR
+% Autor: José Fuentes 
+% Fecha: 2020/06/01
+% Versión: 1.0
+%%
 x=max(max(Im))^2;
 m=(norm(Im-Ref,'fro')^2)/(size(Im,1)*size(Im,2));
 a=10*log10(x/m);
